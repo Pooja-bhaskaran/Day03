1d569c9791e2c3e2653b9265a85135f64be29d1a
unknown <https://github.com/Pooja-bhaskaran/Day03.git>
2022-05-21 17:59:38 +0530

    Employee DailyWage

--- src/com/bridgelabz/Day03/EmpWage.java
+++ src/com/bridgelabz/Day03/EmpWage.java
@@ -1,13 +1,18 @@
 package com.bridgelabz.Day03;
 
 public class EmpWage {
+    static final int WAGE_PER_HOUR = 20;
+    static final int FULL_TIME_HOUR = 8;
     public static void main(String[] args) {
         System.out.println("Welcome to the Employee Wage Computation");
-        int Present = (int) (Math.random() * 10) % 2;
-        int empCheck = 1;
-        if (empCheck == Present) {
+        int empPresent = 1;
+        int empCheck = (int) (Math.random()*10)%2;
+        if (empCheck == empPresent) {
             System.out.println("Employee is Present");
+            int totalWage = WAGE_PER_HOUR * FULL_TIME_HOUR;
+            System.out.println("Daily wage of Employee is : " + totalWage);
         } else
             System.out.println("Employee is absent");
+        
     }
 }
