7701356a9e2b8d4c3e86871f9ea615307fc9dabd
unknown <https://github.com/Pooja-bhaskaran/Day03.git>
2022-05-21 17:13:36 +0530

    Welcome to EmpWage Computation

---
+++ src/com/bridgelabz/Day03/EmpWage.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.Day03;
+
+public class EmpWage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Employee Wage Computation");
+    }
+}
