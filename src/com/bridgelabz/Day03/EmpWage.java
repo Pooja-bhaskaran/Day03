97e7287ef90030913f86832a9bc66f62b8cff80a
unknown <https://github.com/Pooja-bhaskaran/Day03.git>
2022-05-21 17:43:09 +0530

    Employee Attedance check

--- src/com/bridgelabz/Day03/EmpWage.java
+++ src/com/bridgelabz/Day03/EmpWage.java
@@ -3,5 +3,11 @@ package com.bridgelabz.Day03;
 public class EmpWage {
     public static void main(String[] args) {
         System.out.println("Welcome to the Employee Wage Computation");
+        int Present = (int) (Math.random() * 10) % 2;
+        int empCheck = 1;
+        if (empCheck == Present) {
+            System.out.println("Employee is Present");
+        } else
+            System.out.println("Employee is absent");
     }
 }
